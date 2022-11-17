2773e03d9cd6eb5822efa6abcf1fcfd76de7b838
mnitb <nhatminhnguyen1608@gmail.com>
2022-11-17 10:58:43 +0700

    header added!

--- Linear Equation Solver/Complex.h
+++ Linear Equation Solver/Complex.h
@@ -1,1 +1,9 @@
 #pragma once
+struct Complex
+{
+	Complex(double real = 0, double imag = 0);
+	Complex operator+(const Complex& rhs);
+	Complex operator-(const Complex& rhs);
+	Complex operator*(const Complex& rhs);
+	Complex operator/(const Complex& rhs);
+};
