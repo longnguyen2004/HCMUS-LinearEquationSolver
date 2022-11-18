e088cd0b1db10aa607e829e4e422bc89e295052c
Long Nguyen <nguyen.long.908132@gmail.com>
2022-11-18 18:46:16 +0700

    complex: Various small fixes

--- Linear Equation Solver/complex.cpp
+++ Linear Equation Solver/complex.cpp
@@ -10,8 +10,7 @@ void complex_add(double& real, double& imag, double a, double b, double c, doubl
 // Calculate (a + bi) - (c + di)
 void complex_sub(double& real, double& imag, double a, double b, double c, double d)
 {
-	real = a - c;
-	imag = b - d;
+	complex_add(real, imag, a, b, -c, -d);
 }
 
 // Calculate (a + bi) * (c + di)
@@ -40,19 +39,19 @@ int solve_eq_complex(double& realV, double& imagV,
 	complex_sub(realA, imagA, realA, imagA, realC, imagC);
 	complex_sub(realB, imagB, realD, imagD, realB, imagB);
 
-	// 2. Special case: A = 0
+	// 2. Special case: A == 0
 	if (realA == 0 && imagA == 0)
 	{
-		// 2.1 Both coefficients are 0 => Infinite solutions
+		// 2.1 B == 0 => Infinite solutions
 		if (realB == 0 && imagB == 0) return -1;
 
-		// 2.2 Else: No solutions
+		// 2.2 B != 0: No solutions
 		else return 0;
 	}
 	else
 	{
 		// 2.3: x = (D - B)/(A - C)
 		complex_div(realV, imagV, realB, imagB, realA, imagA);
-		return 0;
+		return 1;
 	}
 }
