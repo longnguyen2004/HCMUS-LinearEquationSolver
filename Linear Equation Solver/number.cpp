0341dc946bf634747ff49bd0ea040c5a75af97d3
Hung <you@example.com>
2022-11-18 22:35:44 +0700

    add number module

---
+++ Linear Equation Solver/number.cpp
@@ -0,0 +1,18 @@
+#include "number.h"
+int solve_eq_number(double a, double b, double c, double d, double& x)
+{
+	if ((a - c) == 0)
+	{
+		if ((b - d) == 0)
+			return -1; 	// vo so nghiem tra ve -1
+		else
+			return 0;  // vo nghiem tra ve 0
+	}
+	else
+	{
+		x = (d - b) / (a - c);
+		if (x == -0)
+			x = 0;
+		return 1;       // co nghiem tra ve 1
+	}
+}
---
+++ Linear Equation Solver/number.h
@@ -0,0 +1,2 @@
+#pragma once
+int solve_eq_number(double a, double b, double c, double d, double& x);
