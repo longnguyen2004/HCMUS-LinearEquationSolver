f9b9a5af895051fdf19b9c5840687cecba9801af
Long Nguyen <nguyen.long.908132@gmail.com>
2022-11-20 10:23:46 +0700

    Add solver function template

---
+++ Linear Equation Solver/solver.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Solve Ax + B = Cx + D for any data types
+template <typename T>
+int solve_eq(T& x, T a, T b, T c, T d)
+{
+	// 1. Rearrange: (A - C)x = D - B;
+	a = a - c;
+	b = d - b;
+
+	// 2. Special case: A == 0
+	if (a == 0)
+	{
+		// 2.1. B == 0 => Infinite solutions, else no solutions
+		return b == 0 ? -1 : 0;
+	}
+	else
+	{
+		x = b / a;
+		return 1;
+	}
+}
