30374c87c92f7b0cdf089d26f49b1ed63ec2d3a4
Long Nguyen <nguyen.long.908132@gmail.com>
2022-12-03 18:46:06 +0700

    complex: Remove unnecessary references

--- Linear Equation Solver/complex.cpp
+++ Linear Equation Solver/complex.cpp
@@ -1,43 +1,38 @@
 #include "complex.h"
 
 // Calculate (a + bi) + (c + di)
-void complex_add(Complex& result, Complex a, Complex b)
+Complex complex_add(Complex a, Complex b)
 {
-	result.real = a.real + b.real;
-	result.imag = a.imag + b.imag;
+	return { a.real + b.real, a.imag + b.imag };
 }
 
 // Calculate (a + bi) - (c + di)
-void complex_sub(Complex& result, Complex a, Complex b)
+Complex complex_sub(Complex a, Complex b)
 {
 	b.real = -b.real;
 	b.imag = -b.imag;
-	complex_add(result, a, b);
+	return complex_add(a, b);
 }
 
 // Calculate (a + bi) * (c + di)
-void complex_mul(Complex& result, Complex a, Complex b)
+Complex complex_mul(Complex& result, Complex a, Complex b)
 {
-	result.real = a.real * b.real - a.imag * b.imag;
-	result.imag = a.real * b.imag + a.imag * b.real;
+	return { a.real * b.real - a.imag * b.imag, a.real * b.imag + a.imag * b.real };
 }
 
 // Calculate (a + bi) / (c + di)
-bool complex_div(Complex& result, Complex a, Complex b)
+Complex complex_div(Complex a, Complex b)
 {
-	if (b.real == 0 && b.imag == 0) return false;
 	double den = b.real * b.real + b.imag * b.imag;
-	result.real = (a.real * b.real + a.imag * b.imag) / den;
-	result.imag = (a.imag * b.real - a.real * b.imag) / den;
-	return true;
+	return { (a.real * b.real + a.imag * b.imag) / den, (a.imag * b.real - a.real * b.imag) / den };
 }
 
 // Solve Ax + B = Cx + D, with the coefficients as complex numbers
 int solve_eq_complex(Complex& result, Complex a, Complex b, Complex c, Complex d)
 {
 	// 1. Rearrange: (A - C)x = D - B
-	complex_sub(a, a, c);
-	complex_sub(b, d, b);
+	a = complex_sub(a, c);
+	b = complex_sub(d, b);
 
 	// 2. Special case: A == 0
 	if (a.real == 0 && a.imag == 0)
@@ -51,7 +46,7 @@ int solve_eq_complex(Complex& result, Complex a, Complex b, Complex c, Complex d
 	else
 	{
 		// 2.3: x = (D - B)/(A - C)
-		complex_div(result, b, a);
+		result = complex_div(b, a);
 		return 1;
 	}
 }
